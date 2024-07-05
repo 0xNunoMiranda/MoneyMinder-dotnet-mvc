940fb8162ba9c21bbeb094e13df19691aae98043
Nuno Miranda <nuno_miranda@outlook.com>
2024-07-05 09:49:01 +0100

    first endpoint

---
+++ src/MoneyMinder-dotnet-mvc.API/Controllers/ExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoneyMinder_dotnet_mvc.Communication.Requests;
+
+namespace MoneyMinder_dotnet_mvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterExpense request)
+        {
+            return Created();
+        }
+
+    }
+}
---
+++ src/MoneyMinder-dotnet-mvc.Communication/Enums/PaymentType.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyMinder_dotnet_mvc.Communication.Enums
+{
+    public enum PaymentType
+    {
+        Cash = 0,
+        CreditCard = 1,
+        DebitCard = 2,
+        EletronicTransfer = 3,
+    }
+}
