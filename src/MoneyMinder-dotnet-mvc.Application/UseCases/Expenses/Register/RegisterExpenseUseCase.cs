bb79463fca0d7cfeb86b331d3f5452d8a33aa87f
Nuno Miranda <nuno_miranda@outlook.com>
2024-07-05 12:10:55 +0100

    simple request validation

--- src/MoneyMinder-dotnet-mvc.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/MoneyMinder-dotnet-mvc.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,13 +1,34 @@
-﻿using MoneyMinder_dotnet_mvc.Communication.Requests;
+﻿using MoneyMinder_dotnet_mvc.Communication.Enums;
+using MoneyMinder_dotnet_mvc.Communication.Requests;
 using MoneyMinder_dotnet_mvc.Communication.Responses;
 
-namespace MoneyMinder_dotnet_mvc.Application.UseCases.Expenses.Register
+namespace MoneyMinder_dotnet_mvc.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
 {
-    public class RegisterExpenseUseCase
+    public ResponseRegisteredExpense Execute(RequestRegisterExpense request)
     {
-        public ResponseRegisteredExpense Execute(RequestRegisterExpense request)
-        {
-            return new ResponseRegisteredExpense();
-        }
+        Validate(request);
+        return new ResponseRegisteredExpense();
     }
+    
+    private void Validate(RequestRegisterExpense request)
+    {
+        var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+        if (titleIsEmpty) throw new ArgumentException("The title is required");
+
+        if (request.Amount <= 0) throw new ArgumentException("The Amount must be greater than zero.");
+
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0) throw new ArgumentException("Expenses cannot be for the future");
+        
+        //Enum.isDefined valida o range dos valores estipulados. Caso o valor do input esteja fora dos valores
+        //estipulados no Enum, então irá devolver FALSE.
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType),request.PaymentType);
+        if (!paymentTypeIsValid) throw new ArgumentException("Payment Type is not valid");
+
+
+    }
+
 }
